2213a2e58624c1a7a7873f829110f84513ea3dbc
David Xue <David.Xue37@myhunter.cuny.edu>
2021-10-05 18:17:05 -0400

    fixed error with initial page

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-    x = request.GET #gets the request from the user
-    print(x.__getitem__("product")) #prints the product name the user enters
+    if(request.method == 'GET'): #checks if we can GET
+        x = request.GET.get("product") #gets the product name if possible
+        print(x) #prints the product name the user enters (to check)
     return render(request, 'mainpage.html')
