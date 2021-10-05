e25e5e2b899dd9175ce359b919811717381f6476
David Xue <David.Xue37@myhunter.cuny.edu>
2021-10-05 15:02:44 -0400

    Gets and prints whatever the user enters

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
+    x = request.GET #gets the request from the user
+    print(x.__getitem__("product")) #prints the product name the user enters
     return render(request, 'mainpage.html')
