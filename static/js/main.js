0bfac56b15167ed2ab192c7b16560356383fc59c
etam4225 <etam4225@bths.edu>
2021-10-29 15:23:51 -0400

    Updated mainpage popup function

--- static/js/main.js
+++ static/js/main.js
@@ -2,14 +2,17 @@
 var bbuy_table = document.getElementById("bestbuy_table");
 var popup_data = document.getElementById("fakepopup_data");
 
-function displayInfo(){
+function displayInfo(product_id, productName){
+  console.log(product_id)
   document.getElementById("fakepopup_data").style.display = "block";
-  //popup_data.style.display = "block";
-  console.log("revealingData");
+  document.getElementById("fakepopup_data_main").value = product_id;
+  console.log("New value: " + document.getElementById("fakepopup_data_main").value);
+  document.getElementById("fakepopup_data_main").innerHTML = productName;
+//  console.log("revealingData");
 }
 
 
 function hideDisplayInfo(){
   document.getElementById("fakepopup_data").style.display = "none";
-  console.log("hiding....");
+  //console.log("hiding....");
 }
