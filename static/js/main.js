4f2876feb11a5c3f5da8196a9bdfac95612c75cb
etam4225 <etam4225@bths.edu>
2021-10-30 00:58:01 -0400

    clicking popup displays data - from bestbuy only currently.

--- static/js/main.js
+++ static/js/main.js
@@ -2,12 +2,16 @@
 var bbuy_table = document.getElementById("bestbuy_table");
 var popup_data = document.getElementById("fakepopup_data");
 
-function displayInfo(product_id, productName){
+function displayInfo(product_id, productName, bestbuyData){
   console.log(product_id)
   document.getElementById("fakepopup_data").style.display = "block";
   document.getElementById("fakepopup_data_main").value = product_id;
   console.log("New value: " + document.getElementById("fakepopup_data_main").value);
   document.getElementById("fakepopup_data_main").innerHTML = productName;
+  // {% for x in all_enteries %}
+  // // console.log("values to be added.")
+  // {% endfor %}
+
 //  console.log("revealingData");
 }
 
