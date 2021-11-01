41c69d0699eacd8f41825224a2f479b8cc4b98ad
Danny Li <44373403+DannyLi804@users.noreply.github.com>
2021-10-31 21:45:04 -0400

    Add files via upload

--- pages/views.py
+++ pages/views.py
@@ -226,3 +226,26 @@ def update(StoreName,arr):
                 user_obj.Gamestop_Status=arr[3][i]
             user_obj.save()
     return 0
+"""
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer,format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_plot(x,y):
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(20,10))
+    plt.title('idk')
+    plt.plot(x,y)
+    plt.xticks(rotation=45)
+    plt.xlabel('item')
+    plt.ylabel('price')
+    plt.tight_layout()
+    graph = get_graph()
+    return graph
+"""
