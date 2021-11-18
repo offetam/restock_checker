bffcd70945ef1497d5ab54c1cebd03b2359ce3fb
Danny Li <dannyli804@gmail.com>
2021-11-17 23:05:19 -0500

    Update settings.py

--- restock/settings.py
+++ restock/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = ( os.path.join(BASE_DIR,'staticfiles'), )
 
 STATIC_URL = '/static/'
 
