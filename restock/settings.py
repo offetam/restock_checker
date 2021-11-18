c15cce51c135f655fdcbf89cfa567504d85332a0
Danny Li <dannyli804@gmail.com>
2021-11-17 22:53:09 -0500

    Danny Commit

--- restock/settings.py
+++ restock/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os.path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +164,4 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 if DEBUG:
     import mimetypes
     mimetypes.add_type("application/javascript", ".js", True)
+django_heroku.settings(locals())
