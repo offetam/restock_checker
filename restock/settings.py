81e4b238e187dd0aa0a49dfbeb7d1c9125b71668
Danny Li <dannyli804@gmail.com>
2021-11-17 23:23:15 -0500

    Update settings.py

--- restock/settings.py
+++ restock/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
