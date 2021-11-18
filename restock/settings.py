6591e0c7bf1e0fa0cfebc3b3311f8bb2e033c121
Danny Li <dannyli804@gmail.com>
2021-11-17 23:19:12 -0500

    Update settings.py

--- restock/settings.py
+++ restock/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
