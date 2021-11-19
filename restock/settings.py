eb853d78fbf1a7c33b93f83d9aac36a01804f993
David Xue <David.Xue37@myhunter.cuny.edu>
2021-11-19 14:04:29 -0500

    comment out other database

--- restock/settings.py
+++ restock/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'restock.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -101,7 +101,7 @@ DATABASES={
         'PORT': 5432,
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
