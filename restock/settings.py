066e93518fe97525cc2cb4a15bf337fad0f7c8e7
Danny Li <dannyli804@gmail.com>
2021-11-23 13:58:40 -0500

    Update settings.py

--- restock/settings.py
+++ restock/settings.py
@@ -113,6 +113,9 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
