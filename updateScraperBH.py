a7002025cae6ff881498d23ba329c82ea0f0fffb
AldenL <44271692+AldenL@users.noreply.github.com>
2021-11-28 15:09:19 -0500

    Update updateScraperBH.py

--- updateScraperBH.py
+++ updateScraperBH.py
@@ -17,72 +17,89 @@ driver = webdriver.Chrome(options=chrome_options)
 
 def extract_info(item):
     #get b&h sku
-    sku_parent = item.find("div", "desc_1Tzf71-71iRGoSImRMVVzQ")
-    sku = sku_parent.find("div", "sku_1xEagLq5syWf24ghxMo9-4").text
-    sku = re.findall(r'# (.*) MFR',sku)[0]
-
-    '''
+    sku = str(item.find_all('div',{'data-selenium':'miniProductPageProductSkuInfo'}))
+    try:
+        sku = re.findall(r'-->(.*)<!-- --> ',sku)[0]
+    except IndexError:
+        sku = re.findall(r'# (.*) MFR',sku)[0]
+    
     #find item name
     atag = item.h3.a
     title = atag.text.strip()
-    '''
+    
+    #Find price
     try:
-        #Find price
-        ptag = item.find_all("span", "container_14EdEmSSsYmuetz3imKuAI")
-        tprice = str(ptag)
+        tprice = str(item.find_all("span", {'data-selenium':'uppedDecimalPrice'}))
         uppertag = re.findall(r'First">(.*)<\/span><',tprice)[0]
         lowertag = re.findall(r'Second">(.*)<\/sup>',tprice)[0]
         price = uppertag + "." + lowertag
     except IndexError:
         return
 
+    #get stock info
     try:
-        #get stock info
-        stocktag = item.find_all("span", attrs={"data-selenium": "stockStatus"})
+        stocktag = item.find_all("span", {"data-selenium": "stockStatus"})
         t_stock = str(stocktag)
         stock = re.findall(r'Status">(.*)<\/',t_stock)[0]
     except IndexError:
         return
 
-    '''
     #find img url
     imgtag = item.a.img 
     t_img = str(imgtag)
-    img_url = re.findall(r'src="(.*)"\/>',t_img)[0]
-    '''
-    '''
-    #find item url
-    url_tag = item.find("a","title_ip0F69brFR7q991bIVYh1")
-    url = "https://www.bhphotovideo.com" + url_tag.get("href")
-    '''
+    try:
+        img_url = re.findall(r'src="(.*)"\/>',t_img)[0]
+    except IndexError:
+        img_url = re.findall(r'files\/(.*)"',t_img)[0]
+        img_url = 'https://static.bhphoto.com/images/images345x345/' + img_url
+    
+    
 
+    #find item url
+    url_tag = item.find("a",{'data-selenium':'miniProductPageProductNameLink'})
+    url = "https://www.bhphotovideo.com" + url_tag.get("href")
+    
+
+    
     #get review
     try:
-        review_parent = item.find("a","ratingLink_3h79l-JEkjtWP9MVCF2z3a")
-        reviewlist = review_parent.find_all("svg")
+        review_parent = str(item.find_all("div", {"data-selenium": "miniProductPageProductRatingSection"}))
+        try:
+            reviewlist = re.findall(r'(<svg.*<\/svg>)',review_parent)[0]
+        except IndexError:
+            reviewlist = []
+
+        
+        reviewlist = reviewlist.split('</use></svg>')
+        reviewlist = list(filter(None, reviewlist))
+
         starlist = []
-        for i in reviewlist:
-            star = str(i)
-            starr = re.findall(r'class=\"(.*)\" height', star)[0]
+    
+        for i in reviewlist: 
+            starr = re.findall(r'class=\"(.*)\" height', i)[0]
             starlist.append(starr)
+           
 
         review = 0.0
         for i in starlist:
-            if i == "bhIcon star_3iCe453LqwrKpys0-5fEKx full_3EG7E-E7rKfryMFfiMHoEo":
+            if i == "bhIcon star_FU2un8oJ9r full_FU2un8oJ9r":
                 review += 1
-            elif i == "bhIcon star_3iCe453LqwrKpys0-5fEKx":
+            elif i == "bhIcon star_FU2un8oJ9r":
                 review += 0.5
     except AttributeError:
         review = 0.0
 
+
     #get number of reviews
     try:
-        review_num = item.find("span","reviews_bV6aIRyuom7XD61RdmPUG").text
+        review_num = item.find("span", {"data-selenium": "miniProductPageProductReviews"}).text
         review_num = int(review_num[0])
     except AttributeError:
         review_num = 0
 
-    result = [sku,price,stock,review,review_num]
+    
+    result = [sku, title, price, stock, url, review, review_num, img_url]
+    
     return result
 
 url = "https://www.bhphotovideo.com/c/buy/Graphic-Cards/ci/6567/pn/"
@@ -105,8 +122,7 @@ for i in range(1,10):
 
     time.sleep(5)
     
-for item in records:
-        print(item)
+print(records)
 
 print(len(records))
 #251
@@ -115,9 +131,9 @@ print(len(records))
 
 #REMOVE COMMENTS TO SAVE TO CSV
 #save data to csv
-with open('b&h_update.csv', 'w', newline='', encoding='utf-8') as f:
+with open('b&h_FINAL_img.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(['Sku','Price','Stock','Review','Number_of_Reviews'])
+    writer.writerow(['Sku', 'Name', 'Price','Stock','URL', 'Review','Number_of_Reviews','Image_URL'])
     writer.writerows(records)
     
 
