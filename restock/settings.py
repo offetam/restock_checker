6c5a9125c13b950704a51a182fda52cf75500c77
David Xue <David.Xue37@myhunter.cuny.edu>
2021-11-30 14:53:37 -0500

    testing secret key

--- restock/settings.py
+++ restock/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
