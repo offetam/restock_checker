7d25c54d5652015d72b11d693a27f186745111bb
David Xue <David.Xue37@myhunter.cuny.edu>
2021-12-07 15:05:44 -0500

    set debug to false

--- restock/settings.py
+++ restock/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restockchecker.herokuapp.com','0.0.0.0','127.0.0.1']
 
