e2a47f23c7310793cd091a8a8d036e807706f7b0
David Xue <David.Xue37@myhunter.cuny.edu>
2021-12-10 13:29:47 -0500

    reverted

--- restock/settings.py
+++ restock/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
